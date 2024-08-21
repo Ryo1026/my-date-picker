dcac8b338b5a978299a479e1deac8bd18bfe22ac
Ryo1026 <c9pcv5nvbp@gmail.com>
2024-08-21 23:44:31 +0800

    component layout

---
+++ src/app/datePicker/DatePicker.tsx
@@ -0,0 +1,36 @@
+import style from "./datePicker.module.css";
+import dayjs from "dayjs";
+
+function DatePicker() {
+  const dateObj = dayjs(); // 0- 星期天
+
+  const daysInMonth = dateObj.daysInMonth(); // 當月天數
+
+  const firstWeekStartDay = dateObj.startOf("month").startOf("week");
+
+  const daysArray = [];
+  for (let i = 0; i < 35; i++) {
+    daysArray.push(firstWeekStartDay.add(i, "day"));
+  }
+
+  return (
+    <div className={style.datePicker}>
+      <div className={style.header}>
+        <span>{`<`}</span>
+        <div className={style.currentMonth}>
+          {dateObj.format("YYYY")}年{dateObj.format("M")}月
+        </div>
+        <span>{`>`}</span>
+      </div>
+      <div className={style.dates}>
+        {daysArray.map((el) => (
+          <div key={el.format("YYMMDD")} className={style.day}>
+            {el.format("D")}日
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default DatePicker;
